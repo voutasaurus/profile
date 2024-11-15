36d4395e3a8fd134a937a41ed7ac59bb8d35e267
voutasaurus <anthony.voutas@agworld.com>
2024-11-15 12:25:22 +0800

    make room for lib

--- thimblerig.sh
+++ thimblerig.sh
@@ -1,6 +1,6 @@
 export PREV_WD=`pwd`
 export THIMBLEROOT="$HOME/.thimblerig"
-export THIMBLEPATH="$THIMBLEROOT`pwd`"
+export THIMBLEPATH="$THIMBLEROOT`pwd`/bin"
 
 function augment_path {
     if [ "$PWD" = "$PREV_WD" ]; then return 0; fi;
@@ -8,7 +8,7 @@ function augment_path {
     # Unlist previous thimblepaths
     export PATH=$(echo $PATH | tr ":" "\n" | sed "s|^$THIMBLEROOT.*$||g" | sed "/^[[:space:]]*$/d" | tr "\n" ":")
 
-    export THIMBLEPATH="$THIMBLEROOT`pwd`"
+    export THIMBLEPATH="$THIMBLEROOT`pwd`/bin"
     mkdir -p $THIMBLEPATH
 #	>&2 echo "thimblerig: adding to PATH $THIMBLEPATH"
     export PATH="$THIMBLEPATH:$PATH"
@@ -20,3 +20,9 @@ PROMPT_COMMAND=augment_path # for bash
 precmd() { eval "$PROMPT_COMMAND" } # for zsh
 
 # TODO: system for CRUD executables / links into the THIMBLEPATH
+function rig {
+    local exec_name = $1
+    local exec_path = $2
+
+    ln "$THIMBLEPATH/$exec_name" "$exec_path"
+}
